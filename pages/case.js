c2d6cf7f5ad8e7fe163c02790a258e1349063211
kodomo898 <kodomo893@gmail.com>
2020-11-29 17:25:00 +0900

    fix

--- pages/case.js
+++ pages/case.js
@@ -11,23 +11,20 @@ export default function Case() {
       <section className={utilStyles.headingMd}>
         <div className="anime-type-case">サービス事例</div>
         <div className={utilStyles.box10}>
-            <b>テレライブ </b>
+            <b>テレライブ (Serverside)</b>
             <p>@hidexir</p>
             <img src="/tele.png" alt="テレライブ" className="tele-logo" />
+            <a href="https://tele.live/">https://tele.live/</a>
         </div>
         <div className={utilStyles.box10}>
-            <b>- 各種システム構築</b>
-            <p>(例)予約管理システム(決済システム含む) </p>
-            <p>2,000,000円 ~ (構築含む)(相談可)</p>
-            <p>(例)データ蓄積基盤</p>
-            <p>300,000円 ~ (構築含む)(相談可)</p>
+            <b>BraveFrontier (infrastructure)</b>
+            <p>@ken-z</p>
+            <img src="/brave.png" alt="テレライブ" className="tele-logo" />
         </div>
         <div className={utilStyles.box10}>
-            <b><p>- 監視/運用/保守</p></b>
-            <p>監視のみ </p>
-            <p>10,000円 ~ (相談可)</p>
-            <p>監視/運用/保守</p>
-            <p>50,000円 ~ (相談可)</p>
+            <b>FINAL FANTASY BRAVE EXVIUS (infrastructure)</b>
+            <p>@ken-z</p>
+            <img src="/ex.jpg" alt="テレライブ" className="tele-logo" />
         </div>
 
       </section>
