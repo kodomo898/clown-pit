dfa845b1e1142a85bbd4caa67e66daf76b604491
kodomo898 <kodomo893@gmail.com>
2025-02-19 12:40:36 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -51,10 +51,7 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
+          <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -69,6 +66,9 @@ export default function Home() {
           />
           Examples
         </a>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
     </div>
   );
